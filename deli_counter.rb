efed7e38ed4648c2fe7f9a090852d8ac0414386e
Matt Kuras <mattkuras@gmail.com>
2020-01-25 19:21:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,15 @@
-katz_deli = ["Logan", "Avi", "Spencer"]
 
-def line(katz_deli)
-  if katz_deli.length == 0 
+katz_deli = []
+
+def line_method(numinline)
+  line_method_array = []
+  if numinline.length == 0
     puts "The line is currently empty."
-  else katz_deli.each_with_index {|name, index| puts "The line is currently: #{index +1}. #{name}" }
-end 
-end 
+  else
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
+      # push method will push the name into the index 
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
+  end
+end
