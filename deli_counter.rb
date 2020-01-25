2719d75733b008eb1dce5cd0483959ec83b87c57
Matt Kuras <mattkuras@gmail.com>
2020-01-25 19:26:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 
-def line (katz_deli)
-  if katz_deli.count > 1
-    print "The line is currently:"
-    katz_deli.each_with_index {|name, index|  print " #{index +1}. #{name}" }
-  else
+katz_deli = ["Logan", "Avi", "Spencer"]
+
+def line(katz_deli)
+  if katz_deli.length == 0 
     puts "The line is currently empty."
-  end
-end
+  else puts "The line is currently:" katz_deli.each_with_index {|name, index|  #{index +1}. #{name}" }
+end 
+end 
