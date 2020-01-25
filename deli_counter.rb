8fa4f97a5c35acfddf50106435f36ad3c7025134
Matt Kuras <mattkuras@gmail.com>
2020-01-25 19:51:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, customer)
-  katz_deli << customer
-  
+  katz_deli.push(name)
+	puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
