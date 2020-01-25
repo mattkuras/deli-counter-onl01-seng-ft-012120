367b9d2b0e7bf2d9c36732054564af8f7be0454b
Matt Kuras <mattkuras@gmail.com>
2020-01-25 20:42:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length >= 1 
   puts "Currently serving #{katz_deli[0]}."
-  katz_deli.
+  katz_deli.shift 
  else puts "There is nobody waiting to be served!"
  end 
  end
